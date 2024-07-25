da6e0941a6e7f79754444daaabc35c388d01165c
LennartSch09 <lennartschepelmann@gmail.com>
2024-07-25 08:29:07 +0200

    save passwords individually

--- menu.py
+++ menu.py
@@ -46,24 +46,94 @@ def save_user(username, password):
     with open("user.json", "w") as file:
         json.dump(users, file, indent=4)
 
-def login(stdscr):
-    # Einloggen mit Username und Passwort
-    temp1 = get_input(stdscr, "Please log in using your username and password. Press any key to continue.")
-    username = get_input(stdscr, "Enter username: ")
-    password = get_input(stdscr, "Enter password: ")
+def validate_user(username, password):
+    if os.path.exists("user.json"):
+        with open("user.json", "r") as file:
+            users = json.load(file)
+            for user in users:
+                if user["username"] == username and user["password"] == password:
+                    return True
+    return False
+
+def save_password(username, site, password):
+    filename = f"{username}_passwords.json"
+    password_data = {
+        "site": site,
+        "password": password
+    }
+
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            passwords = json.load(file)
+    else:
+        passwords = []
+
+    passwords.append(password_data)
+
+    with open(filename, "w") as file:
+        json.dump(passwords, file, indent=4)
+
+def add_password(stdscr, username):
+    site = get_input(stdscr, "Enter the name/web-URL/site you want to add: ")
+    password = get_input(stdscr, "Enter the password: ")
+
+    save_password(username, site, password)
 
     stdscr.clear()
-    stdscr.addstr(1, 0, f"Username: {username}")
-    stdscr.addstr(2, 0, f"Password: {password}")
-    stdscr.addstr(3, 0, "Press any key to return to the menu.")
+    stdscr.addstr(1, 0, "Password saved!")
+    stdscr.addstr(2, 0, "Press any key to return to the manager menu.")
     stdscr.refresh()
     stdscr.getch()
 
-def register(stdscr):
-    # Registrieren mit Username und Passwort
-    temp1 = get_input(stdscr, "Please add a username and a password. Press any key to continue.")
+def manager_menu(stdscr, username):
+    menu = ["Add password", "Find password", "Edit password", "Generate password", "Back to main menu"]
+    current_row_idx = 0
+
+    while True:
+        print_menu(stdscr, current_row_idx, menu)
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and current_row_idx > 0:
+            current_row_idx -= 1
+        elif key == curses.KEY_DOWN and current_row_idx < len(menu) - 1:
+            current_row_idx += 1
+        elif key == ord('\n'):
+            selected_option = menu[current_row_idx]
+            if selected_option == "Back to main menu":
+                break
+            elif selected_option == "Add password":
+                add_password(stdscr, username)
+            else:
+                stdscr.clear()
+                stdscr.addstr(0, 0, f"You selected '{selected_option}'.")
+                stdscr.addstr(1, 0, "This feature is not implemented yet.")
+                stdscr.addstr(2, 0, "Press any key to return to the manager menu.")
+                stdscr.refresh()
+                stdscr.getch()
+        print_menu(stdscr, current_row_idx, menu)
+
+def login(stdscr):
+#    get_input(stdscr, "Please log in using your username and password. Press any key to continue.")
     username = get_input(stdscr, "Enter username: ")
     password = get_input(stdscr, "Enter password: ")
+
+    if validate_user(username, password):
+        stdscr.clear()
+        stdscr.addstr(1, 0, "Login successful!")
+        stdscr.refresh()
+        stdscr.getch()
+        manager_menu(stdscr, username)
+    else:
+        stdscr.clear()
+        stdscr.addstr(1, 0, "Invalid username or password.")
+        stdscr.addstr(3, 0, "Press any key to return to the menu.")
+        stdscr.refresh()
+        stdscr.getch()
+
+def register(stdscr):
+#    get_input(stdscr, "Please add a username and a password. Press any key to continue.")
+    username = get_input(stdscr, "Enter your new username: ")
+    password = get_input(stdscr, "Enter your new password: ")
 
     save_user(username, password)
 
