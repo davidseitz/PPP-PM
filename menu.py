af500afe6ca340e74b638989151625b2e8e05725
LennartSch09 <lennartschepelmann@gmail.com>
2024-07-25 07:52:54 +0200

    save_user

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import curses
-#GitPushTest
+import json
+import os
 
 def print_menu(stdscr, selected_row_idx, menu):
     stdscr.clear()
@@ -28,9 +29,26 @@ def get_input(stdscr, prompt):
     curses.noecho()  # Disable input echoing
     return user_input
 
-def einloggen(stdscr):
+def save_user(username, password):
+    user_data = {
+        "username": username,
+        "password": password
+    }
+
+    if os.path.exists("user.json"):
+        with open("user.json", "r") as file:
+            users = json.load(file)
+    else:
+        users = []
+
+    users.append(user_data)
+
+    with open("user.json", "w") as file:
+        json.dump(users, file, indent=4)
+
+def login(stdscr):
     # Einloggen mit Username und Passwort
-    temp1 = get_input(stdscr, "Bitte Loggen Sie sich mit Username und Passwort ein. Drücken Sie eine beliebige Taste um Fortzufahren")
+    temp1 = get_input(stdscr, "Please log in using your username and password. Press any key to continue.")
     username = get_input(stdscr, "Enter username: ")
     password = get_input(stdscr, "Enter password: ")
 
@@ -41,16 +59,18 @@ def einloggen(stdscr):
     stdscr.refresh()
     stdscr.getch()
 
-def registrieren(stdscr):
+def register(stdscr):
     # Registrieren mit Username und Passwort
-    temp1 = get_input(stdscr, "Bitte legen Sie einen Usernamen und ein Passwort fest. Drücken Sie eine beliebige Taste um Fortzufahren")
+    temp1 = get_input(stdscr, "Please add a username and a password. Press any key to continue.")
     username = get_input(stdscr, "Enter username: ")
     password = get_input(stdscr, "Enter password: ")
+
+    save_user(username, password)
 
     stdscr.clear()
     stdscr.addstr(1, 0, f"Username: {username}")
     stdscr.addstr(2, 0, f"Password: {password}")
-    stdscr.addstr(3, 0, "Press any key to return to the menu.")
+    stdscr.addstr(3, 0, "New User Saved! Press any key to return to the menu.")
     stdscr.refresh()
     stdscr.getch()
 
@@ -58,7 +78,7 @@ def main(stdscr):
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    menu = ['Einloggen', 'Neuer Benutzer', 'Exit']
+    menu = ['Log In', 'New User', 'Exit']
     current_row_idx = 0
 
     while True:
@@ -73,10 +93,10 @@ def main(stdscr):
             selected_option = menu[current_row_idx]
             if selected_option == 'Exit':
                 break
-            elif selected_option == 'Einloggen':
-                einloggen(stdscr)
-            elif selected_option == 'Neuer Benutzer':
-                registrieren(stdscr)
+            elif selected_option == 'Log In':
+                login(stdscr)
+            elif selected_option == 'New User':
+                register(stdscr)
             else:
                 stdscr.clear()
                 stdscr.addstr(0, 0, f"You selected '{selected_option}'.")
