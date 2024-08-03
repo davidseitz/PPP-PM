bf4f3cff026d07091f34a9d7c24c72147735c663
davidseitz <seitz.david-it23@it.dhbw-ravensburg.de>
2024-08-03 15:21:32 +0200

    check passwor critiria

---
+++ tests/testCheckPassword.py
@@ -0,0 +1,38 @@
+"""
+This file contains the tests for the checkPassword.py file.
+"""
+import unittest
+from source.checkPassword import checkPassword, _checkLength, _checkLowercase, \
+    _checkUppercase, _checkDigit, _checkSpecial, _checkPwned
+
+class TestCheckPassword(unittest.TestCase):
+    """
+    This class contains the tests for the checkPassword.py file.
+    """
+    def testCheckPassword(self) -> None:
+        self.assertTrue(checkPassword("Test1234!!@sadadad42311231."))
+
+    def testCheckLength(self) -> None:
+        self.assertTrue(_checkLength("Test1234!234"))
+        self.assertTrue(_checkLength("Iam13characte"))
+        self.assertFalse(_checkLength("Test1234"))
+
+    def testCheckLowercase(self) -> None:
+        self.assertTrue(_checkLowercase("Test1234!"))
+        self.assertFalse(_checkLowercase("TEST1234!"))
+
+    def testCheckUppercase(self) -> None:
+        self.assertTrue(_checkUppercase("Test1234!"))
+        self.assertFalse(_checkUppercase("test1234!"))
+
+    def testCheckDigit(self) -> None:
+        self.assertTrue(_checkDigit("Test1234!"))
+        self.assertFalse(_checkDigit("Test!"))
+
+    def testCheckSpecial(self) -> None:
+        self.assertTrue(_checkSpecial("Test1234!"))
+        self.assertFalse(_checkSpecial("Test1234"))
+
+    def testCheckPwned(self) -> None:
+        self.assertTrue(_checkPwned("Test1234!@sadadad42311231."))
+        self.assertFalse(_checkPwned("12345"))
--- tests/testMain.py
+++ tests/testMain.py
@@ -4,6 +4,7 @@ This file is used to run all the tests in the tests folder
 import unittest
 from .testCheckPwned import TestHaveIBeenPwned
 from .testMenu import testMenu as TestMenu
+from .testCheckPassword import TestCheckPassword
 
 def testMain() -> None:
     """
@@ -22,6 +23,15 @@ def testMain() -> None:
     suite.addTest(TestMenu('test_validate_user_fail'))
     suite.addTest(TestMenu('test_save_password_update'))
     suite.addTest(TestMenu('test_save_password_new'))
+
+    #CheckPassword tests
+    suite.addTest(TestCheckPassword('testCheckPassword'))
+    suite.addTest(TestCheckPassword('testCheckLength')) 
+    suite.addTest(TestCheckPassword('testCheckLowercase'))
+    suite.addTest(TestCheckPassword('testCheckUppercase'))
+    suite.addTest(TestCheckPassword('testCheckDigit'))
+    suite.addTest(TestCheckPassword('testCheckSpecial'))
+    suite.addTest(TestCheckPassword('testCheckPwned'))
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
