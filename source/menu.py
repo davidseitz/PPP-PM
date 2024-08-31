b0e31e567753de63d3fad29202ca64685ccc5b2a
davidseitz <seitz.david-it23@it.dhbw-ravensburg.de>
2024-08-31 11:53:56 +0200

    Updated requirements.txt

--- source/menu.py
+++ source/menu.py
@@ -677,7 +677,7 @@ def main(stdscr: curses.window) -> None:
                     else:
                         stdscr.clear()
                         stdscr.addstr(1, 0, "Invalid code.")
-                        stdscr.addstr(2, 0, "Are you're clocks synced up?")
+                        stdscr.addstr(2, 0, "Are you're clocks synced?")
                         stdscr.addstr(3, 0, "Press any key to return to the Login menu.")
                         stdscr.refresh()
                         stdscr.getch()
